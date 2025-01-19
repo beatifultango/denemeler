50b4de5e43c8185c0623ba6852a4d142c5b65ee9
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-01-19 11:53:02 +0300

    calculaor tamamlandı

--- js/calculator.js
+++ js/calculator.js
@@ -1,51 +1,55 @@
 //değişkenleri tanımla
 
+let hesapla = true;
 let result;
-let hesapla=true;
 
 function calculate() {
-  while (hesapla){
-    let firstNumber= prompt(`İlk sayınızı giriniz:`);
-    if (firstNumber===null){
-        alert(`Calculator is off.`);
-        hesapla=false;
-        break;
+  while (hesapla) {
+    let firstNumber = prompt(`İlk sayınızı giriniz:`);
+    if (firstNumber === null) {
+      alert(`Calculator is off.`);
+      hesapla = false;
+      break;
     }
-    if(firstNumber<0 || typeof firstNumber!="number"){
-        alert(`Geçerli bir sayı giriniz.`);
-        continue
-
+    firstNumber = Number(firstNumber);
+    if (firstNumber < 0 || isNaN(firstNumber)) {
+      alert(`Geçerli bir sayı giriniz.`);
+      continue;
     }
-
-    if(firstNumber,secondNumber){
-        if(firstNumber+ "+"+secondNumber){
-            result.push(firstNumber+secondNumber);
-            break
-            
-        }else if(firstNumber+ "-"+secondNumber){
-            result.push(firstNumber-secondNumber);
-            break
-            
-        }else if(firstNumber+ "*"+secondNumber){
-            result.push(firstNumber*secondNumber);
-            break
-            
-        }else if(firstNumber+ "/"+secondNumber){
-            result.push(firstNumber/secondNumber);
-            break
-            
-        }else{
-            return `geçerli bir sayı giriniz.`;
-            
-        }
-
+    let secondNumber = prompt(`İkinci sayıyı giriniz:`);
+    secondNumber = Number(secondNumber);
+    if (secondNumber < 0 || isNaN(secondNumber)) {
+      alert(`Geçerli bir sayı giriniz.`);
+      continue;
     }
 
-    
-    
+    let calculationMark = prompt(`Yapmak istediğiniz işlemi giriniz:`);
+    if (
+      calculationMark != "+" &&
+      calculationMark != "-" &&
+      calculationMark != "*" &&
+      calculationMark != "/"
+    ) {
+      alert(`geçerli bir işlem giriniz.`);
+      continue;
+    }
+
+    if (calculationMark === "+") {
+      result = firstNumber + secondNumber;
+    }
+    if (calculationMark === "-") {
+      result = firstNumber - secondNumber;
+    }
+    if (calculationMark === "*") {
+      result = firstNumber * secondNumber;
+    }
+    if (calculationMark === "/") {
+      result = firstNumber / secondNumber;
+    }
+    alert(`Sonuç: ${result}`);
+    hesapla = false;
   }
-  return result;
 }
 
-alert(`Hesaplamak istediğiniz sayıları giriniz.`)
+alert(`Hesaplamak istediğiniz sayıları giriniz.`);
 calculate();
